808fec59b7e2678ee0d2146cc26aeeed39d931ec
Harald Wilbertz <harald.wilbertz@gmx.de>
2021-03-31 12:15:31 +0200

    Readline added.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-#! /
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+readline.question(`What would you like to log today? `, item => {
+    console.log(item);
+    readline.close();
+})
